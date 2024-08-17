2c355ab67b6b264c95398afd2c5d97e5408cb73c
Raman7913 <raman7913523@gmail.com>
2024-08-17 16:10:50 +0545

    added random numbers function and fixed ace logic

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
-while (true) {
-    let firstCard = Math.ceil(Math.random() * 10) + 1;
-    let secondCard = Math.ceil(Math.random() * 10) + 1;
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+const startBtn = document.querySelector(".start-btn");
+const messageEl = document.querySelector("#message-el");
+const cardsEl = document.querySelector(".cards");
+const sumEl = document.querySelector(".sum-el");
+const newEl = document.querySelector(".new");
 
-    let sum = firstCard + secondCard;
-    console.log(`${firstCard} + ${secondCard} = ${sum}`);
-    if (sum === 21) {
-        console.log("🍻 You won Blackjack!!!");
-        break;
-    } else if (sum <= 21) {
-        console.log("at least you didn't lose. Do you wanna pick again?");
-        if (prompt("press Y or N") != "Y") {
-            break;
-        }
+startBtn.addEventListener("click", startGame);
+newEl.addEventListener("click", newCard);
+
+function startGame() {
+    let firstCard = getRandomNumber();
+    let secondCard = getRandomNumber();
+    sum = firstCard + secondCard;
+    cards = [firstCard, secondCard];
+    isAlive = true;
+    renderGame();
+}
+
+function renderGame() {
+    sumEl.textContent = "Sum: " + sum;
+    cardsEl.textContent = "Cards:";
+    for (i = 0; i < cards.length; i++) {
+        cardsEl.textContent += " " + cards[i];
+    }
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "Wohoo? You've got Blackjack!";
+        hasBlackJack = true;
     } else {
-        console.log("you lost!😭😭😭😭😭");
-        break;
+        message = "You're out of the game";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+}
+
+function newCard() {
+    if (isAlive && !hasBlackJack) {
+        let card = getRandomNumber();
+        cards.push(card);
+        sum += card;
+        renderGame();
+    }
+}
+
+function getRandomNumber() {
+    //get random number between 1 and 13 inclusive
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber === 1) {
+        return 11;
+    } else if (randomNumber > 10) {
+        return 10;
+    } else {
+        return randomNumber;
     }
 }
